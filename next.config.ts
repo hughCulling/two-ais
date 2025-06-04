c279d7462eaa8a2ffe705d0a156e0b9379eb642d
Hugh Wilfred Culling <hughculling@icloud.com>
2025-06-04 13:01:56 +0100

    resolved warning in terminal

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': ['@svgr/webpack'],
+      },
+    },
   },
 };
 
