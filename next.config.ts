3748fe1a095cfca9bfc5dfb66f8c4cb47f56d26c
Hugh Wilfred Culling <hughculling@icloud.com>
2025-07-10 11:50:30 +0100

    added Grok 4

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'img.youtube.com',
+    ],
+  },
   experimental: {
     turbo: {
       rules: {
